fa2eefd3cb9aa07aeb0e299d8d2bc77e3dab8d76
punk73 <eng70@JVC-JEIN.CO.ID>
2018-01-15 16:56:15 +0700

    adding event handler for variable empty string

--- app/Http/Controllers/mainController.php
+++ app/Http/Controllers/mainController.php
@@ -24,6 +24,7 @@ class mainController extends Controller
         if( $req->tanggal != null ){
 
             $do = $do->where('tanggal','=', $req->tanggal ) ;
+            // return $req->tanggal;
         }
 
         //pagination
@@ -91,52 +92,47 @@ class mainController extends Controller
 
     public function store(Request $req)
     {
-    	# code...
-    	$line_name = $req->line_name;
-    	$time = $req->time;
-    	$minute = $req->minute;
-    	$target_sop = $req->target_sop;
-    	$osc_output = $req->osc_output;
-    	$plus_minus = $req->plus_minus;
-    	$lost_hour = $req->lost_hour;
-    	
-        //delay type changes to column
-
-        $delay_type = $req->delay_type;
-
-    	$problem = $req->problem;
-    	$dic = $req->dic; //department in charge
-    	$action = $req->action;
-    	$users_id = $req->users_id;
-    	$shift = $req->shift;
-    	$tanggal = $req->tanggal;
 
     	$Daily_output = new Daily_output;
-
-    	$Daily_output->line_name = $line_name;
-    	$Daily_output->time = $time;
-    	$Daily_output->minute = $minute;
-    	$Daily_output->target_sop = $target_sop;
-    	$Daily_output->osc_output = $osc_output;
-    	$Daily_output->plus_minus = $plus_minus;
-    	$Daily_output->lost_hour = $lost_hour;
+    	$Daily_output->line_name = $req->input('line_name', null);
+    	$Daily_output->time = $req->input('time', null);
+    	$Daily_output->minute = $req->input('minute', null);
+    	$Daily_output->target_sop = $req->input('target_sop', null);
+    	$Daily_output->osc_output = $req->input('osc_output', null);
+    	$Daily_output->plus_minus = $req->input('plus_minus', null);
+    	$Daily_output->lost_hour = $req->input('lost_hour', null);
         //new column
-        $Daily_output->board_delay = $req->board_delay;
-        $Daily_output->part_delay = $req->part_delay;
-        $Daily_output->eqp_trouble = $req->eqp_trouble;
-        $Daily_output->quality_problem_delay = $req->quality_problem_delay;
-        $Daily_output->bal_problem = $req->bal_problem;
-        $Daily_output->others = $req->others;
-        $Daily_output->support = $req->support;
-        $Daily_output->change_model = $req->change_model;
+        $Daily_output->board_delay = $req->input('board_delay', null);
+        $Daily_output->part_delay = $req->input('part_delay', null);
+        $Daily_output->eqp_trouble = $req->input('eqp_trouble', null);
+        $Daily_output->quality_problem_delay = $req->input('quality_problem_delay', null);
+        $Daily_output->bal_problem = $req->input('bal_problem', null);
+        $Daily_output->others = $req->input('others', null);
+        $Daily_output->support = $req->input('support', null);
+        $Daily_output->change_model = $req->input('change_model', null);
         //end new column
-    	$Daily_output->delay_type = $delay_type;
-        $Daily_output->problem = $problem;
-    	$Daily_output->dic = $dic;
-    	$Daily_output->action = $action;
-    	$Daily_output->users_id = $users_id;
-    	$Daily_output->shift = $shift;
-    	$Daily_output->tanggal = $tanggal;
+    	$Daily_output->delay_type = $req->input('delay_type', null);
+        $Daily_output->problem = $req->input('problem', null);
+    	$Daily_output->dic = $req->input('dic', null);
+    	$Daily_output->action = $req->input('action', null);
+    	$Daily_output->users_id = $req->input('users_id', null);
+    	$Daily_output->shift = $req->input('shift', null);
+    	$Daily_output->tanggal = $req->input('tanggal', null);
+
+        if(!$Daily_output->minute){$Daily_output->minute = null; }
+        if(!$Daily_output->target_sop){$Daily_output->target_sop = null; }
+        if(!$Daily_output->osc_output){$Daily_output->osc_output = null; }
+        if(!$Daily_output->plus_minus){$Daily_output->plus_minus = null; }
+        if(!$Daily_output->lost_hour){$Daily_output->lost_hour = null; }
+        if(!$Daily_output->board_delay){$Daily_output->board_delay = null; }
+        if(!$Daily_output->part_delay){$Daily_output->part_delay = null; }
+        if(!$Daily_output->eqp_trouble){$Daily_output->eqp_trouble = null; }
+        if(!$Daily_output->quality_problem_delay){$Daily_output->quality_problem_delay = null; }
+        if(!$Daily_output->bal_problem){$Daily_output->bal_problem = null; }
+        if(!$Daily_output->others){$Daily_output->others = null; }
+        if(!$Daily_output->support){$Daily_output->support = null; }
+        if(!$Daily_output->change_model){$Daily_output->change_model = null; }
+        if(!$Daily_output->users_id){$Daily_output->users_id = null; }
 
     	$Daily_output->save();
 
@@ -191,14 +187,39 @@ class mainController extends Controller
     	$Daily_output->plus_minus = $req->plus_minus;
     	$Daily_output->lost_hour = $req->lost_hour;
         //new column
-        $Daily_output->board_delay = $req->board_delay;
-        $Daily_output->part_delay = $req->part_delay;
-        $Daily_output->eqp_trouble = $req->eqp_trouble;
-        $Daily_output->quality_problem_delay = $req->quality_problem_delay;
-        $Daily_output->bal_problem = $req->bal_problem;
-        $Daily_output->others = $req->others;
-        $Daily_output->support = $req->support;
-        $Daily_output->change_model = $req->change_model;
+        if( !empty($req->board_delay) )
+        {$Daily_output->board_delay = $req->board_delay;}
+        else {$Daily_output->board_delay = 0;}
+        
+        if( !empty($req->part_delay) )
+        {$Daily_output->part_delay = $req->part_delay;}
+        else{$Daily_output->part_delay = 0;}
+
+        if( !empty($req->eqp_trouble) )
+        {$Daily_output->eqp_trouble = $req->eqp_trouble;}
+        else{$Daily_output->eqp_trouble = 0;}
+
+        if( !empty($req->quality_problem_delay) )
+        {$Daily_output->quality_problem_delay = $req->quality_problem_delay;}
+        else{$Daily_output->quality_problem_delay = 0;}
+
+        if( !empty($req->bal_problem) )
+        {$Daily_output->bal_problem = $req->bal_problem;}
+        else{$Daily_output->bal_problem = 0;}
+        
+        if( !empty($req->others) )
+        {$Daily_output->others = $req->others;} 
+        else{$Daily_output->others = 0;}
+        
+        if( !empty($req->support) )
+        {$Daily_output->support = $req->support;}
+        else{$Daily_output->support = 0;}
+
+        
+        if( !empty($req->change_model) )
+        {$Daily_output->change_model = $req->change_model;}
+        else{$Daily_output->change_model = 0;}
+
         //end new column
     	$Daily_output->delay_type = $req->delay_type;
     	$Daily_output->problem = $req->problem;
@@ -207,6 +228,23 @@ class mainController extends Controller
     	$Daily_output->users_id = $req->users_id;
     	$Daily_output->shift = $req->shift;
     	$Daily_output->tanggal = $req->tanggal;
+
+        //error handler
+            if(!$Daily_output->minute){$Daily_output->minute = null; }
+            if(!$Daily_output->target_sop){$Daily_output->target_sop = null; }
+            if(!$Daily_output->osc_output){$Daily_output->osc_output = null; }
+            if(!$Daily_output->plus_minus){$Daily_output->plus_minus = null; }
+            if(!$Daily_output->lost_hour){$Daily_output->lost_hour = null; }
+            if(!$Daily_output->board_delay){$Daily_output->board_delay = null; }
+            if(!$Daily_output->part_delay){$Daily_output->part_delay = null; }
+            if(!$Daily_output->eqp_trouble){$Daily_output->eqp_trouble = null; }
+            if(!$Daily_output->quality_problem_delay){$Daily_output->quality_problem_delay = null; }
+            if(!$Daily_output->bal_problem){$Daily_output->bal_problem = null; }
+            if(!$Daily_output->others){$Daily_output->others = null; }
+            if(!$Daily_output->support){$Daily_output->support = null; }
+            if(!$Daily_output->change_model){$Daily_output->change_model = null; }
+            if(!$Daily_output->users_id){$Daily_output->users_id = null; }
+        //END ERROR HANDLER
 
         if ( $Daily_output != null ){
             $Daily_output->save();
